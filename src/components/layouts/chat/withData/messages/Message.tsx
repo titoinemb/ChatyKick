753b5c396b28adbd482b23386a52137e4fe373fd
titoinemb <mb_titoine@proton.me>
2026-01-05 20:08:12 -0500

    fix: clip-... in tsx

--- src/components/layouts/chat/withData/messages/Message.tsx
+++ src/components/layouts/chat/withData/messages/Message.tsx
@@ -17,77 +17,82 @@ export const Message: React.FC<MessageProps> = ({ item, handleMouseEnter, handle
     username
   } = useUser();
 
-  if (typeof item.metadata === "string") {
-    metadata = JSON.parse(item.metadata);
-  };
-
-  console.log(metadata)
+  if (typeof item.metadata === "string") metadata = JSON.parse(item.metadata);
 
   return (
     <>
       {visible && (
         <User badges={badges!} followingSince={followingSince!} profilePic={profilePic!} username={username!} removeUserPopup={removeUserPopup} />
       )}
-      <div className="msg"
-        onMouseEnter={() => {
-          if (handleMouseEnter) {
-            handleMouseEnter(item.id)
-          }
-        }}
-        onMouseLeave={() => {
-          if (handleMouseLeave) {
-            handleMouseLeave(item.id)
-          }
-        }}
-      >
-        {metadata && metadata.original_sender && (
-          <div className="replyMetadata">
-            Replying to {metadata.original_sender.username!}:
-            <span className="content" dangerouslySetInnerHTML={{ __html: replaceEmotesAndLinks(metadata.original_message.content!) }} />
-          </div>
-        )}
-        
-        <div className="identity">
-          <div className="badges">
-            {item.sender.identity.badges.map((badge, i) => {
-              if (!(badge.text in BadgeMap)) return null;
-
-              let BadgeComponent = BadgeMap[badge.text as keyof typeof BadgeMap];
-
-              var dark = "#fff";
-              var light = "#fff";
-
-              function isBadgeType(type: string): type is BadgeTypes {
-                return type in SubGiftBadgesColor;
-              };
-
-              if (isBadgeType(badge.type)) {
-                let badgeColor = SubGiftBadgesColor[badge.type];
-
-                dark = badgeColor.dark;
-                light = badgeColor.light;
-              };
-
-              return <div className="badge" key={i}><BadgeComponent dark={dark} light={light} /></div>;
-            })}
-          </div>
-          <div className="username"
-            onClick={() => {
-              getUserInfo(channelName, item.sender.username)
+      {metadata?.celebration
+      ? /** show celebration message (sub) */
+      (
+        <>sub</>
+      )
+      : /** show normal message (reply/message) */
+      (
+        <div className="msg"
+          onMouseEnter={
+            () => {
+              if (handleMouseEnter) {
+                handleMouseEnter(item.id)
+              }
             }}
-            style={{ color: item.sender.identity.color }}
-          >
-            {item.sender.username}
-            <span>:</span>
+          onMouseLeave={() => {
+            if (handleMouseLeave) {
+              handleMouseLeave(item.id)
+            }
+          }}
+        >
+          {metadata && metadata.original_sender && (
+            <div className="replyMetadata">
+              Replying to {metadata.original_sender.username!}:
+              <span className="content" dangerouslySetInnerHTML={{ __html: replaceEmotesAndLinks(metadata.original_message.content!) }} />
+            </div>
+          )}
+
+          <div className="identity">
+            <div className="badges">
+              {item.sender.identity.badges.map((badge, i) => {
+                if (!(badge.text in BadgeMap)) return null;
+
+                let BadgeComponent = BadgeMap[badge.text as keyof typeof BadgeMap];
+
+                var dark = "#fff";
+                var light = "#fff";
+
+                function isBadgeType(type: string): type is BadgeTypes {
+                  return type in SubGiftBadgesColor;
+                };
+
+                if (isBadgeType(badge.type)) {
+                  let badgeColor = SubGiftBadgesColor[badge.type];
+
+                  dark = badgeColor.dark;
+                  light = badgeColor.light;
+                };
+
+                return <div className="badge" key={i}><BadgeComponent dark={dark} light={light} /></div>;
+              })}
+            </div>
+            <div className="username"
+              onClick={() => {
+                getUserInfo(channelName, item.sender.username)
+              }}
+              style={{ color: item.sender.identity.color }}
+            >
+              {item.sender.username}
+              <span>:</span>
+            </div>
           </div>
-        </div>
-        <span className="content" dangerouslySetInnerHTML={{ __html: html }} />
-        {item.sender.id !== Number(localStorage.getItem("channelId")) && replyMode && (
-          <div className="reply" id={item.id} onClick={() => replyMode(item)}>
-            <Reply />
-          </div>
-        )}
-      </div>
+          <span className="content" dangerouslySetInnerHTML={{ __html: html }} />
+          {item.sender.id !== Number(localStorage.getItem("channelId")) && replyMode && (
+            <div className="reply" id={item.id} onClick={() => replyMode(item)}>
+              <Reply />
+            </div>
+          )}
+        </div >
+      )}
     </>
   );
 };
--- src/components/svg/badges/Subgifter.tsx
+++ src/components/svg/badges/Subgifter.tsx
@@ -3,7 +3,7 @@ import { SubGiftProps } from "@types";
 
 export const SubGifter: React.FC<SubGiftProps> = ({ dark, light }) => (
     <svg viewBox="0 0 16 16" width="20px" height="20px" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <g clip-path="url(#clip0_301_17810)">
+      <g>
         <path d="M7.99999 9.14999V6.62499L0.484985 3.35999V6.34499L1.15499 6.63499V12.73L7.99999 15.995V9.14999Z" fill={dark} />
         <path d="M8.00003 10.735V9.61501L1.15503 6.63501V7.70501L8.00003 10.735Z" fill={dark} />
         <path d="M15.515 3.355V6.345L14.85 6.64V12.73L12.705 13.755L11.185 14.48L8.00499 15.995V6.715L4.81999 5.295H4.81499L3.29499 4.61L0.484985 3.355L3.66999 1.935L3.67999 1.93L5.09499 1.3L8.00499 0L10.905 1.3L12.32 1.925L12.33 1.935L15.515 3.355Z" fill={light} />
